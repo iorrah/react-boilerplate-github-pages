11a637b6284f6f16c4b0723a5d245119f50f926a
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-21 15:56:11 +0200

    Updates

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ var __wpo = {
       "/react-boilerplate-github-pages/npm.redux-saga.c2dcc97d94d690026a8d.chunk.js",
       "/react-boilerplate-github-pages/npm.webpack.9e56360a500dbf4fc704.chunk.js",
       "/react-boilerplate-github-pages/3.7525977cb98c4c79d020.chunk.js",
-      "/react-boilerplate-github-pages/main.0a3e299c273ed4fc3006.chunk.js",
+      "/react-boilerplate-github-pages/main.d721d18d0f551e9a300b.chunk.js",
       "/react-boilerplate-github-pages/npm.babel.5b4af551a71d021be59e.chunk.js",
       "/react-boilerplate-github-pages/npm.connected-react-router.5e775e0a549b0d4c0bec.chunk.js",
       "/react-boilerplate-github-pages/npm.core-js.f2526b90a1b08370b85d.chunk.js",
@@ -36,7 +36,7 @@ var __wpo = {
     "52013361977eb200baa1b175c7efde133498ea95": "/react-boilerplate-github-pages/npm.redux-saga.c2dcc97d94d690026a8d.chunk.js",
     "a32b8c4d069544d3731ecbfedfcdba7bb465e691": "/react-boilerplate-github-pages/npm.webpack.9e56360a500dbf4fc704.chunk.js",
     "c678f84c8e22d3a8d87132cf3d7ffb2df226da04": "/react-boilerplate-github-pages/3.7525977cb98c4c79d020.chunk.js",
-    "1a72ef29b7f2479b79381df4717822c7eba75385": "/react-boilerplate-github-pages/main.0a3e299c273ed4fc3006.chunk.js",
+    "8f60156add8939dcf76708cde306743b4bdcf0fa": "/react-boilerplate-github-pages/main.d721d18d0f551e9a300b.chunk.js",
     "d7c0a38979bfc5728a6d6300fc7afe2c0416ce94": "/react-boilerplate-github-pages/npm.babel.5b4af551a71d021be59e.chunk.js",
     "072b973d1e2cf97cea1576cf4df644fbf9b449fc": "/react-boilerplate-github-pages/npm.connected-react-router.5e775e0a549b0d4c0bec.chunk.js",
     "fd399f080bb167c6ea2cb78933536143cf973c09": "/react-boilerplate-github-pages/npm.core-js.f2526b90a1b08370b85d.chunk.js",
@@ -51,11 +51,11 @@ var __wpo = {
     "c84539bc29aceb9aff2f1d513a09fb4e8d72db2c": "/react-boilerplate-github-pages/16.f7da5ddedfa582133045.chunk.js",
     "17425a569854f13a4a336e07d90f57b8f989daf5": "/react-boilerplate-github-pages/17.050dd15ae5141dcb56fe.chunk.js",
     "1dee67f1c16ef55a66548aae426a4a91b5a3ce61": "/react-boilerplate-github-pages/18.b7493a252e5054a14a3c.chunk.js",
-    "724c1d71493fa7952de48dca4a130523b697d91b": "/react-boilerplate-github-pages/"
+    "b08e2c68b9059e7626a9dafb496a1ae0bd0ba972": "/react-boilerplate-github-pages/"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "4/21/2020, 3:52:14 PM",
+  "version": "4/21/2020, 3:56:09 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
